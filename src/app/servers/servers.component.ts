dee02030a02ecc392b9920c8ea44cae7432bdda1
Sahil <gsahil307@gmail.com>
2024-01-08 18:01:46 +0530

    L30 All databinding forms

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,14 +9,23 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = "No server created.";
   serverCount = 0;
+  serverName = '';
+
   onCreateServer(){
-    this.serverCreationStatus = "Server was created ";
+    this.serverCreationStatus = "Server was created. Name is " + this.serverName;
     this.serverCount++;
   }
+
   constructor(){
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000)
   }
 
+  onUpdateServerName(event){
+    console.log(event);
+    this.serverName = event.target.value;
+    
+  }
 }
+
